8418f47f6beb028f0ae512d12e03e309fcac4e2b
Amirzhan Aliyev <letavocado@proton.me>
2024-02-16 19:12:22 +0600

    feat: add astro

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+  redirects: [{ from: "/", to: "./public/Amirzhan_Aliyev_SE_2024.docx" }],
+});
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SiteTitle = "Let Avocado!";
+export const SiteDescription = "Welcome to my green golden blog!";
