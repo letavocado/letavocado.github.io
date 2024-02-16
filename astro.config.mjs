7fc44d31bb0c85d07f39bf4ff8e16477b0be8a6f
Amirzhan Aliyev <letavocado@proton.me>
2024-02-16 19:17:58 +0600

    fix: site link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://letavocado.github.io",
   integrations: [
     tailwind({
       applyBaseStyles: false,
